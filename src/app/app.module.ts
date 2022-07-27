fc7600e3ee7be1ca1a7439ee26e41d955e34ba47
rsantoyo-dev <ricardo.santoyo@hotmail.com>
2022-07-27 09:23:38 -0400

    5 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SuperJssModule } from 'super-jss';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, SuperJssModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    MainContentComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { SJssTheme } from 'super-jss';
+import { ThemeHandlerService } from '../theme/theme-handler.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  theme: SJssTheme;
+  constructor(private themeService: ThemeHandlerService) {
+    this.theme = themeService._theme;
+    themeService.getTheme().subscribe((t) => {
+      this.theme = t;
+    });
   }
 
+  ngOnInit() {}
+  colorClicked() {
+    let th: SJssTheme | null = { ...this.theme };
+
+    if (th.palette.primary.main === '#ff3366') {
+      th.palette.primary.main = '#003366';
+      this.themeService.setTheme(th);
+      return;
+    }
+    th.palette.primary.main = '#ff3366';
+  }
 }
