4c1cc4b36427601bfab1fb07b9378077921075d3
rsantoyo-dev <ricardo.santoyo@hotmail.com>
2022-07-27 17:10:50 -0400

    2 files modified

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { SJssTheme } from 'super-jss';
+import { ThemeHandlerService } from '../theme/theme-handler.service';
 
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
-  styleUrls: ['./main-content.component.css']
+  styleUrls: ['./main-content.component.css'],
 })
 export class MainContentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  theme: SJssTheme;
+  constructor(private themeService: ThemeHandlerService) {
+    this.theme = themeService._theme;
+    themeService.getTheme().subscribe((t) => {
+      this.theme = t;
+    });
   }
 
+  ngOnInit() {}
 }
